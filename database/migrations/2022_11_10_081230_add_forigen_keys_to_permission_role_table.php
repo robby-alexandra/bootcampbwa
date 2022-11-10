31841487cb5725c34fb9114401f3a74dff534f1c
robby-alexandra <robbyalex60@gmail.com>
2022-11-10 16:35:11 +0700

    clear All DB

--- database/migrations/2022_11_10_081230_add_forigen_keys_to_permission_role_table.php
+++ database/migrations/2022_11_10_081230_add_forigen_keys_to_permission_role_table.php
@@ -15,6 +15,8 @@ class AddForigenKeysToPermissionRoleTable extends Migration
     {
         Schema::table('permission_role', function (Blueprint $table) {
             //
+            $table->foreign('permission_id', 'fk_permission_role_to_permission')->references('id')->on('permission')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('role_id', 'fk_permission_role_to_role')->references('id')->on('role')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -27,6 +29,8 @@ class AddForigenKeysToPermissionRoleTable extends Migration
     {
         Schema::table('permission_role', function (Blueprint $table) {
             //
+            $table->dropForeign('fk_permission_role_to_permission');
+            $table->dropForeign('fk_permission_role_to_role');
         });
     }
 }
---
+++ database/migrations/2022_11_10_092418_add_forign_keys_to_appointment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForignKeysToAppointmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('appointment', function (Blueprint $table) {
+            //
+            $table->foreign('doctor_id', 'fk_appointment_to_specialist')->references('id')->on('specialist')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id', 'fk_appointment_to_users')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('consultation_id', 'fk_appointment_to_consultation')->references('id')->on('consultation')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('appointment', function (Blueprint $table) {
+            //
+            $table->dropForeign('fk_appointment_to_specialist');
+            $table->dropForeign('fk_appointment_to_consultation');
+            $table->dropForeign('fk_appointment_to_users');
+        });
+    }
+}
