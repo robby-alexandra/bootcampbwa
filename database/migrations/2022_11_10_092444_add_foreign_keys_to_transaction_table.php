4fabc87bf51601c0c77cc7425a158cd7c3e8a401
robby-alexandra <robbyalex60@gmail.com>
2022-11-18 11:11:55 +0700

    perbaruhi

--- database/migrations/2022_11_10_092444_add_foreign_keys_to_transaction_table.php
+++ database/migrations/2022_11_10_092444_add_foreign_keys_to_transaction_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddForignKeysToTransactionTable extends Migration
+class AddForeignKeysToTransactionTable extends Migration
 {
     /**
      * Run the migrations.
